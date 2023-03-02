df09352992ab04a2419e476a82c080665ee6ca57
Alexey Bobkov <xxxtrihardxxx@gmail.com>
2023-03-02 14:30:41 +0200

    initial commit

---
+++ src/main/java/com/example/lidlpoisk/LidlPoiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.lidlpoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LidlPoiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LidlPoiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lidlpoisk/LidlPoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lidlpoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LidlPoiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
